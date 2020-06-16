7c40793db1349c066d812e71f71917e46df878eb
uoft-chiplab <thywissenlab.rpi02@gmail.com>
2020-06-16 11:08:54 -0400

    Fixed version number bug for V16.4.8

--- vars.h
+++ vars.h
@@ -20,7 +20,7 @@ typedef int BOOL;
 ADwin Variables and GUI things
 *************************************************************************/
 
-#define SEQUENCER_VERSION "ADwin Sequencer V16.4.7 - "
+#define SEQUENCER_VERSION "ADwin Sequencer V16.4.8 - "
 
 // ADwin info
 #define DefaultEventPeriod (0.100)   // in milliseconds
