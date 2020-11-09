5381188d803f0411f437ad809480fb56c5b12e06
Colin Dale <thywissenlab.rpi02@gmail.com>
2020-11-09 16:43:12 -0500

    Changed version name LOL

--- vars.h
+++ vars.h
@@ -20,7 +20,7 @@ typedef int BOOL;
 ADwin Variables and GUI things
 *************************************************************************/
 
-#define SEQUENCER_VERSION "ADwin Sequencer V16.9.0 - "
+#define SEQUENCER_VERSION "ADwin Sequencer V17.0.0 - "
 
 // ADwin info
 #define DefaultEventPeriod (0.100)   // in milliseconds
