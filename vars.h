c1ce41de345c8531882941d996d3c4390d90e326
Colin Dale <thywissenlab.rpi02@gmail.com>
2023-11-30 10:38:27 -0500

    Changed Version to 17.1.2

--- vars.h
+++ vars.h
@@ -20,7 +20,7 @@ typedef int BOOL;
 ADwin Variables and GUI things
 *************************************************************************/
 
-#define SEQUENCER_VERSION "ADwin Sequencer V17.1.1 - "
+#define SEQUENCER_VERSION "ADwin Sequencer V17.1.2 - "
 
 // ADwin info
 #define DefaultEventPeriod (0.100)   // in milliseconds
